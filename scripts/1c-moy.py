d97c2bcddd38a75a305c9a4ad22b5731a8406a4a
yusahora <felixod@icloud.com>
2018-11-05 18:40:41 +0100

    mise a jour 2a-mol

---
+++ scripts/1c-moy.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+#1c-moy
+#creer un dictionnaire de nom/note et ressort les 5 meilleurs note
+#15/10/2018
+#Felix Ollivier Drolshagen
+import re
+import operator
+
+#on creer une regex pour verifier les entrees utilisateurs
+reg = re.compile('^[a-zA-Z]+$')
+reg2 = re.compile('^[0-9]+')
+
+Dict ={}
+best = []
+loop = 0
+sum = 0
+while loop == 0:
+    print("saisissez un prenom")
+    nom = input()
+    if reg.match(nom):
+        if nom == "q":
+            for liste in sorted(Dict):
+                best = Dict[liste]
+                sum += int(best)
+            print("moyenne est égale a ")
+            moy = round (sum / len(Dict), 2)
+            print(moy)
+            print("liste des notes")
+            print(sorted(Dict.items(), key=operator.itemgetter(1), reverse = True)[:5])
+            break
+    
+        else:
+
+            print("saisissez votre note")
+            note = input()
+            if reg2.match(note):
+                try:
+                    note = int(note)
+                except ValueError:
+                    pass
+                Dict[nom] = note
+            else:
+                print("n'est pas un nombre")
+    else:
+        print("n'est pas un")
--- scripts/2a-mol.py
+++ scripts/2a-mol.py
@@ -17,12 +17,47 @@ import re
 #on créé les variables nécessaires
 reg = re.compile('^[0-9]+')
 solution = random.randint(0, 100)
+end = False
 print(solution)
-fichierW = open("jeu.txt", "w")
-fichierW.write("bienvenue dans le jeu du plus ou moins")
-fichierW = open("jeu.txt", "r")
-#on créé la boucle dans lequel sera le jeu
-print(fichierW.read())
-#while fichier.read() != solution:
-#    fichier.write(input)
-#fichier.close
+#on créé la fonction de fin
+def fin():
+    print("Au revoir, merci d'avoir jouer")
+    exit()
+#on créé la fonction pour écrire dans un fichier
+def write_file(msg):
+  file = open("jeu.txt", "w")
+  file.write(msg)
+  file.close()
+#on créé la fonction pour lire dans le fichier
+def read_file():
+  file = open("jeu.txt", "r")
+  msg = file.read()
+  file.close()
+  return msg
+#fonction anti ctrlC 
+def stop_ctrlC(sig, frame):
+    write_file('Pas ouf de CTRL+C ')
+    exit()
+
+signal.signal(signal.SIGINT, stop_ctrlC)
+
+write_file("Bienvenue dans le jeu du plus ou moins, veuillez entrez un chiffre entre 0 et 100")
+
+#on initialise la boucle pour le jeu
+while end is False :
+    #On va lire dans le file 
+    saisi = read_file()
+     #On vérifie la saisie utilisateur
+    if re.match("^[0-9]+$", saisi):
+        saisi = int(saisi)
+        if saisi > 100:
+            continue          
+        if saisi > solution :
+            write_file('Trop grand !')       
+        elif saisi < solution :
+            write_file('Trop petit !')
+        #Sinon on affiche le message de victoire et on arrete la boucle avec end=True 
+        else :
+            write_file('Gg a toi ! ')
+            end = True
+            fin()
